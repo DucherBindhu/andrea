9ce7224892d922912c90c8e02ea3e64564002bbd
DucherBindhu <binauro9@gmail.com>
2020-08-26 16:51:16 +0200

    Project Front End Andrea Rocagel

---
+++ js/script.js
@@ -0,0 +1,548 @@
+var menuresponsive = document.getElementById("menuresponsive");
+var menu = document.getElementById("menu");
+
+menu.addEventListener('click',function(){
+    if(menuresponsive.style.display=='block'){
+        menuresponsive.style.display='none';
+    }else{
+        menuresponsive.style.display="block";
+    }
+})
+
+var hybrids = document.getElementById('hybrids');
+ var hybrid1 = document.getElementById('hybrid1');
+ var hybrid2 = document.getElementById('hybrid2');
+ 
+ hybrids.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+   
+    if( hybrid1.style.display=="block"){
+        hybrid1.style.display="none";
+    }else{
+        hybrid1.style.display="block";
+    }
+     
+ 
+ })
+ hybrids.addEventListener('click',function(){
+     if( hybrid2.style.display=="block"){
+        hybrid2.style.display="none";
+     }else{
+        hybrid2.style.display="block";
+     }
+      
+  
+  })
+ var isoles = document.getElementById('isoles');
+ var isole1 = document.getElementById('isole1');
+ var isole2 = document.getElementById('isole2');
+ 
+ isoles.addEventListener('click',function(){
+   hybrid1.style.display='none';
+   hybrid2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( isole1.style.display=="block"){
+        isole1.style.display="none";
+    }else{
+        isole1.style.display="block";
+    }
+     
+ 
+ })
+ isoles.addEventListener('click',function(){
+     if( isole2.style.display=="block"){
+        isole2.style.display="none";
+     }else{
+        isole2.style.display="block";
+     }
+      
+  
+  })
+
+var couleur = document.getElementById('couleur');
+var color = document.getElementById('color');
+var couleurs = document.getElementById('couleurs');
+
+couleurs.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( color.style.display=="block"){
+     color.style.display="none";
+    }else{
+     color.style.display="block";
+    }
+     
+ 
+ })
+ couleurs.addEventListener('click',function(){
+    if( couleur.style.display=="block"){
+     couleur.style.display="none";
+    }else{
+     couleur.style.display="block";
+    }
+     
+ 
+ })
+/*couleurs.addEventListener('mouseout',function(){
+    this.style.fontWeight="bold";
+ })*/
+var couleur1 = document.getElementById('couleur1');
+var color1 = document.getElementById('color1');
+
+couleurs.addEventListener('click',function(){
+   if( couleur1.style.display=="block"){
+    couleur1.style.display="none";
+   }else{
+    couleur1.style.display="block";
+   }
+    
+
+})
+couleurs.addEventListener('click',function(){
+    if( color1.style.display=="block"){
+     color1.style.display="none";
+    }else{
+     color1.style.display="block";
+    }
+     
+ 
+ })
+
+ var vagues = document.getElementById('vagues');
+ var vague1 = document.getElementById('vague1');
+ var vague2 = document.getElementById('vague2');
+ 
+ vagues.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( vague1.style.display=="block"){
+        vague1.style.display="none";
+    }else{
+        vague1.style.display="block";
+    }
+     
+ 
+ })
+ vagues.addEventListener('click',function(){
+     if( vague2.style.display=="block"){
+        vague2.style.display="none";
+     }else{
+        vague2.style.display="block";
+     }
+      
+  
+  })
+
+  var repousses = document.getElementById('repousses');
+ var repou = document.getElementById('repou');
+ var repous = document.getElementById('repous');
+ 
+ repousses.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( repou.style.display=="block"){
+        repou.style.display="none";
+    }else{
+        repou.style.display="block";
+    }
+     
+ 
+ })
+ repousses.addEventListener('click',function(){
+     if( repous.style.display=="block"){
+        repous.style.display="none";
+     }else{
+        repous.style.display="block";
+     }
+      
+  
+  })
+  var repou1 = document.getElementById('repou1');
+ var repous1 = document.getElementById('repous1');
+ 
+ repousses.addEventListener('click',function(){
+    if( repou1.style.display=="block"){
+        repou1.style.display="none";
+    }else{
+        repou1.style.display="block";
+    }
+     
+ 
+ })
+ repousses.addEventListener('click',function(){
+     if( repous1.style.display=="block"){
+        repous1.style.display="none";
+     }else{
+        repous1.style.display="block";
+     }
+      
+  
+  })
+  var theories = document.getElementById('theories');
+ var theory = document.getElementById('theory');
+ var theory1 = document.getElementById('theory1');
+ 
+ theories.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( theory.style.display=="block"){
+        theory.style.display="none";
+    }else{
+        theory.style.display="block";
+    }
+     
+ 
+ })
+ theories.addEventListener('click',function(){
+     if( theory1.style.display=="block"){
+        theory1.style.display="none";
+     }else{
+        theory1.style.display="block";
+     }
+      
+  
+  })
+  var friches = document.getElementById('friches');
+ var friche = document.getElementById('friche');
+ var friche1 = document.getElementById('friche1');
+ var friche3 = document.getElementById('friche3');
+ var friche4 = document.getElementById('friche4');
+ 
+ friches.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   hybrid1.style.display="none";
+   hybrid2.style.display="none";
+
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( friche.style.display=="block"){
+      friche.style.display="none";
+    }else{
+      friche.style.display="block";
+    }
+     
+ 
+ })
+ friches.addEventListener('click',function(){
+     if( friche1.style.display=="block"){
+      friche1.style.display="none";
+     }else{
+      friche1.style.display="block";
+     }
+      
+  
+  })
+  friches.addEventListener('click',function(){
+   if( friche3.style.display=="block"){
+     friche3.style.display="none";
+   }else{
+     friche3.style.display="block";
+   }
+    
+
+})
+friches.addEventListener('click',function(){
+    if( friche4.style.display=="block"){
+     friche4.style.display="none";
+    }else{
+     friche4.style.display="block";
+    }
+     
+ 
+ })
+  var monstres = document.getElementById('monstres');
+ var monstre1 = document.getElementById('monstre1');
+ var monstre2 = document.getElementById('monstre2');
+ var monstre3 = document.getElementById('monstre3');
+ var monstre5 = document.getElementById('monstre5');
+ 
+ monstres.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   hybrid1.style.display="none";
+   hybrid2.style.display="none";
+
+   erreur.style.display="none";
+   erreur1.style.display="none";
+
+   
+    if( monstre3.style.display=="block"){
+      monstre3.style.display="none";
+    }else{
+      monstre3.style.display="block";
+    }
+     
+ 
+ })
+ monstres.addEventListener('click',function(){
+     if( monstre5.style.display=="block"){
+      monstre5.style.display="none";
+     }else{
+      monstre5.style.display="block";
+     }
+      
+  
+  })
+  monstres.addEventListener('click',function(){
+   
+    if( monstre1.style.display=="block"){
+      monstre1.style.display="none";
+    }else{
+      monstre1.style.display="block";
+    }
+     
+ 
+ })
+ monstres.addEventListener('click',function(){
+     if( monstre2.style.display=="block"){
+      monstre2.style.display="none";
+     }else{
+      monstre2.style.display="block";
+     }
+      
+  
+  })
+  var erreurs = document.getElementById('erreurs');
+ var erreur = document.getElementById('erreur');
+ var erreur1 = document.getElementById('erreur1');
+ 
+ erreurs.addEventListener('click',function(){
+   isole1.style.display='none';
+   isole2.style.display="none";
+   color.style.display="none";
+   couleur.style.display="none";
+   color1.style.display="none";
+   couleur1.style.display="none";
+   vague1.style.display="none";
+   vague2.style.display="none";
+   repou.style.display="none";
+   repous.style.display="none";
+   repou1.style.display="none";
+   repous1.style.display="none";
+   theory1.style.display="none";
+   theory.style.display="none";
+   friche.style.display="none";
+   friche1.style.display="none";
+   friche4.style.display="none";
+   friche3.style.display="none";
+   monstre1.style.display="none";
+   monstre2.style.display="none";
+   monstre3.style.display="none";
+   monstre5.style.display="none";
+   hybrid1.style.display="none";
+   hybrid2.style.display="none";
+
+   
+    if( erreur.style.display=="block"){
+      erreur.style.display="none";
+    }else{
+      erreur.style.display="block";
+    }
+     
+ 
+ })
+ erreurs.addEventListener('click',function(){
+     if( erreur1.style.display=="block"){
+      erreur1.style.display="none";
+     }else{
+      erreur1.style.display="block";
+     }
+      
+  
+  })
+
+  // cibile button acquérir
+  var acquerir = document.getElementById('acquerir') ;
+
+  var nombreItems = document.getElementById('nombreItems');
+  let panier =  localStorage.getItem('compteur') ? JSON.parse(localStorage.getItem('compteur')) : 0 ;
+
+
+  localStorage.setItem('compteur', JSON.stringify(panier));
+
+
+nombreItems.innerHTML = localStorage.getItem('compteur') + " " + " articles dans panier";
+
+
+acquerir.addEventListener('click',function(){
+   console.log('test')
+   panier ++;
+
+   localStorage.setItem('compteur', JSON.stringify(panier))
+   nombreItems.innerHTML = localStorage.getItem('compteur') + " " + "articles dans panier";
+
+
+})
+
+  
+ 
